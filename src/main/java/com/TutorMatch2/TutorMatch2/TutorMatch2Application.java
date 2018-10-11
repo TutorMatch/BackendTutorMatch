ee7f2b03f28d394f6db2921bd4b84a8284d9d577
Hiromi Akita <vanesahiromi@gmail.com>
2018-10-11 11:13:48 -0500

    Se creó la base para el proyecto

---
+++ src/main/java/com/TutorMatch2/TutorMatch2/TutorMatch2Application.java
@@ -0,0 +1,12 @@
+package com.TutorMatch2.TutorMatch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorMatch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorMatch2Application.class, args);
+	}
+}
---
+++ src/test/java/com/TutorMatch2/TutorMatch2/TutorMatch2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.TutorMatch2.TutorMatch2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorMatch2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
